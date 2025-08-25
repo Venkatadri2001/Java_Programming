be11bc71197237ed8885c7c8ad63c2973bc57733
Venkatadri Majjari <142674720+Venkatadri2001@users.noreply.github.com>
2025-08-25 16:45:06 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package cscorner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.print("I am Venky ");
+		System.out.print("'I am Fresher'");
+
+	}
+
+}
